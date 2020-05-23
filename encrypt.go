a223ff15c96d68667afaa722fc6324f99cb04c12
David Zech <david@davidzech.com>
2020-05-23 00:14:45 -0700

    Fill out ncrypt.Encrypt

--- encrypt.go
+++ encrypt.go
@@ -1,26 +1,7 @@
 package ncrypt
 
-import (
-	"errors"
-	"reflect"
-)
+import "crypto/cipher"
 
-// encryptStruct encrypts a pointer to a Struct
-func (c *Context) encryptStruct(pStruct interface{}) error {
-	v := reflect.ValueOf(pStruct)
-	if v.Kind() == reflect.Ptr && v.Elem().Kind() == reflect.Struct {
-		s := v.Elem()
-		// TODO: make sure the embedded fields are mutually exclusive
-		if f := s.FieldByName("Encrypt"); !f.IsZero() {
-			// found field, check if its the right type
-			if f.Type() == reflect.TypeOf(Encrypt{}) {
-				enc := f.Interface().(Encrypt)
-
-			}
-		}
-		if !s.FieldByName("Seal").IsZero() {
-			return errors.New("not implemented yet")
-		}
-	}
-	return errors.New("value is not a pointer")
+func encryptStruct(c cipher.Stream, target interface{}) error {
+	return nil
 }
--- ncrypt.go
+++ ncrypt.go
@@ -1,5 +1,14 @@
 package ncrypt
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
 type Encrypter interface {
 	Encrypt(interface{}) error
 }
@@ -16,24 +25,77 @@ const (
 )
 
 type Context struct {
-	Key interface{}
+	Key  []byte
+	Mode Mode
+}
+
+func (c *Context) key() []byte {
+	return c.Key
 }
 
 func (c *Context) Decrypt(target interface{}) error {
 	return nil
 }
 
+func makeIV(len int) (iv []byte, err error) {
+	if err := func() error {
+		iv = make([]byte, len)
+		read, err := rand.Reader.Read(iv)
+		if err != nil {
+			return err
+		}
+		if read != len {
+			return errors.New("failed to read random bytes")
+		}
+		return nil
+	}(); err != nil {
+		return nil, fmt.Errorf("failed to generate IV: %v", err)
+	}
+	return
+}
+
 func (c *Context) Encrypt(target interface{}) error {
-	return nil
+	v := reflect.ValueOf(target)
+	if v.Kind() == reflect.Ptr && v.Elem().Kind() == reflect.Struct {
+		s := v.Elem()
+		// TODO: make sure the embedded fields are mutually exclusive
+		if f := s.FieldByName("Encrypt"); !f.IsZero() {
+			// found field, check if its the right type
+			if f.Type() == reflect.TypeOf(Encrypt{}) {
+				enc := f.Interface().(Encrypt)
+				if enc.Encrypted {
+					return errors.New("already encrypted")
+				}
+				block, err := aes.NewCipher(c.key()) // do key
+				if err != nil {
+					return fmt.Errorf("failed to initialize block cipher: %v", err)
+				}
+				iv, err := makeIV(block.BlockSize())
+				if err != nil {
+					return err
+				}
+				stream := cipher.NewCTR(block, iv)
+				if err := encryptStruct(stream, target); err != nil {
+					return fmt.Errorf("failed to encrypt struct: %w", err)
+				}
+			}
+		}
+		if !s.FieldByName("Seal").IsZero() {
+			return errors.New("not implemented yet")
+		}
+	}
+	return errors.New("value is not a pointer")
 }
 
 type AEADMetadata struct {
-	Nonce   []byte
-	AuthTag []byte
+	Encrypted bool
+	Nonce     []byte
+	AuthTag   []byte
 }
 
 type StreamMetadata struct {
-	IV []byte
+	Encrypted bool
+	IV        []byte
 }
 
 type Seal struct {
