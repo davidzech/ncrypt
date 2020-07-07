f833b8bc975e09088b543cf17bf8cc90146dba49
David Zech <david@davidzech.com>
2020-07-06 22:47:04 -0700

    More whatever

--- encrypt.go
+++ encrypt.go
@@ -3,7 +3,74 @@ package ncrypt
 import (
 	"crypto/cipher"
 	"reflect"
+	"unsafe"
 )
 
 func encryptStruct(c cipher.Stream, s reflect.Value) {
+	// we know s is a pointer to a struct, therefore all values can be set
+	for i := 0; i < s.NumField(); i++ {
+		field := s.Field(i)
+		switch field.Kind() {
+		case reflect.Int:
+			bytes := (*[unsafe.Sizeof(int(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Int64:
+			bytes := (*[unsafe.Sizeof(int64(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Int32:
+			bytes := (*[unsafe.Sizeof(int32(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Int16:
+			bytes := (*[unsafe.Sizeof(int16(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Int8:
+			bytes := (*[unsafe.Sizeof(int8(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Uint:
+			bytes := (*[unsafe.Sizeof(uint(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Uint64:
+			bytes := (*[unsafe.Sizeof(uint64(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Uint32:
+			bytes := (*[unsafe.Sizeof(uint32(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Uint16:
+			bytes := (*[unsafe.Sizeof(uint16(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Uint8:
+			bytes := (*[unsafe.Sizeof(uint8(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Uintptr:
+			bytes := (*[unsafe.Sizeof(uintptr(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Float64:
+			bytes := (*[unsafe.Sizeof(float64(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Float32:
+			bytes := (*[unsafe.Sizeof(float32(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Complex64:
+			bytes := (*[unsafe.Sizeof(complex64(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.Complex128:
+			bytes := (*[unsafe.Sizeof(complex128(0))]byte)(unsafe.Pointer(field.UnsafeAddr()))[:] // potentially unsafe
+			c.XORKeyStream(bytes, bytes)
+		case reflect.String:
+			bytes := []byte(field.String()) // strings are immutable so hacks are required to do this
+			c.XORKeyStream(bytes, bytes)
+			field.SetString(string(bytes))
+		case reflect.Slice:
+
+		}
+
+	}
+}
+
+func encryptInt(c cipher.Stream, v *int) {
+
+}
+
+func decryptStruct(c cipher.Stream, s reflect.Value) {
+	encryptStruct(c, s)
 }
--- encrypt_test.go
+++ encrypt_test.go
@@ -1,27 +1,66 @@
 package ncrypt
 
-import "testing"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"math"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestEncryptStruct(t *testing.T) {
 	type primitives struct {
 		Encrypt
 
-		String     string
-		Int        int
-		Int64      int64
-		Int32      int32
-		Int16      int16
-		Uint       uint
-		Uint64     uint64
-		Uint32     uint32
-		Uint16     uint16
-		Uintptr    uintptr
-		Rune       rune
-		Byte       byte
+		String  string
+		Int     int
+		Int64   int64
+		Int32   int32
+		Int16   int16
+		Int8    int8
+		Uint    uint
+		Uint64  uint64
+		Uint32  uint32
+		Uint16  uint16
+		Uint8   uint8
+		Uintptr uintptr
+		// Rune       rune // alias for int32
+		// Byte       byte // alias for uint8
 		Float64    float64
 		Float32    float32
 		Complex64  complex64
 		Complex128 complex128
 	}
+
+	s := primitives{
+		String:     "hello",
+		Int:        -42,
+		Int64:      math.MaxInt64,
+		Int32:      math.MaxInt32,
+		Int16:      math.MaxInt16,
+		Int8:       0x6d,
+		Uint:       42,
+		Uint64:     math.MaxUint64,
+		Uint32:     math.MaxUint32,
+		Uint16:     math.MaxUint16,
+		Uintptr:    uintptr(0x100),
+		Float64:    math.MaxFloat64,
+		Float32:    math.MaxFloat32,
+		Complex64:  complex(float32(1), float32(1)),
+		Complex128: complex(float64(1), float64(1)),
+	}
+	notASecret := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	b, _ := aes.NewCipher(notASecret)
+	stream := cipher.NewCTR(b, notASecret)
+	encryptStruct(stream, reflect.ValueOf(&s).Elem())
+	stream = cipher.NewCTR(b, notASecret)
+	decryptStruct(stream, reflect.ValueOf(&s).Elem())
+	assert.Equal(t, s.Int, -42)
+	assert.Equal(t, s.String, "hello")
+}
+
+func TestEncryptStructNested(t *testing.T) {
 
 }
